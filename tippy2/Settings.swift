f4b3744a8f7dab5c3b9017450ab15c91b848866f
Spencer <spencer.edwards@sjsu.edu>
2019-12-22 17:20:00 -0800

    what

---
+++ tippy2/Settings.swift
@@ -0,0 +1,51 @@
+//
+//  Settings.swift
+//  tippy2
+//
+//  Created by Spencer on 12/22/19.
+//  Copyright © 2019 Spencer. All rights reserved.
+//
+
+import UIKit
+
+class Settings: UIViewController {
+
+    @IBOutlet weak var percentageController: UISegmentedControl!
+    //  @IBOutlet weak var test: UISegmentedControl!
+  //  @IBOutlet weak var percentageController: UISegmentedControl!
+    // @IBOutlet weak var percentageController: UISegmentedControl!
+   // @IBOutlet weak var peopleController: UISegmentedControl!
+   // let defaults = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  //  @IBAction func percentageChanged(_ sender: Any)
+  //  {
+       // let percentArray = [ 0.10, 0.15, 0.20, 0.25,]
+       // let seg = percentageController.selectedSegmentIndex
+       // defaults.set(percentArray[seg], forKey: "myPercent")
+       // defaults.synchronize()
+  //  }
+
+  //  @IBAction func peopleChanged(_ sender: Any)
+   // {
+       // let peopleArray = [ 1, 2, 3, 4, 5, 6, 7, 8,]
+       // let peeps = peopleController.selectedSegmentIndex
+       // defaults.set(peopleArray[peeps], forKey: "myPeople")
+      //  defaults.synchronize()
+    //}
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
